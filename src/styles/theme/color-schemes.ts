b6f57eb290b54e46eb79f3fdb71bc1caa90f944d
yash777u <yogeshkumardagar7@gmail.com>
2024-04-06 20:04:57 +0530

    Done with the Error

--- src/styles/theme/color-schemes.ts
+++ src/styles/theme/color-schemes.ts
@@ -10,7 +10,7 @@ export const colorSchemes = {
       background: {
         default: 'var(--mui-palette-neutral-950)',
         defaultChannel: '9 10 11',
-        paper: 'var(--mui-palette-neutral-900)',
+        paper: '#000000',                                   // this needed to be chnaged, Example value in recognized format
         level1: 'var(--mui-palette-neutral-800)',
         level2: 'var(--mui-palette-neutral-700)',
         level3: 'var(--mui-palette-neutral-600)',
@@ -76,7 +76,7 @@ export const colorSchemes = {
       background: {
         default: 'var(--mui-palette-common-white)',
         defaultChannel: '255 255 255',
-        paper: 'var(--mui-palette-common-white)',
+        paper: '#ffffff',                             // this needed to be chnaged, Example value in recognized format
         level1: 'var(--mui-palette-neutral-50)',
         level2: 'var(--mui-palette-neutral-100)',
         level3: 'var(--mui-palette-neutral-200)',
